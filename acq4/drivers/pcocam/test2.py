002edd61d53eb894556cd0d0dbc6b8e4c04a47cf
YounesB <younesboumaaz@gmail.com>
2015-08-04 17:07:59 +0200

    Update test2.py

--- acq4/drivers/pcocam/test2.py
+++ acq4/drivers/pcocam/test2.py
@@ -6,12 +6,13 @@ import matplotlib.pyplot as plt
 
 pco = pcocam._PCOCameraClass()
 
+#pco.list_Params()
 #cam_hand = pco.open_camera()
 pco.setup_camera()
-pco.record_images(12)
-t = pco.return_images(3)
-#a =plt.imshow(t)
+pco.record_images(3)
+t = pco.return_images(1)
+print t
+#a =plt.imshow(t[0])
 #plt.show()
 #pco.stop_camera(cam_hand)
 #pco.close_camera(cam_hand)
-
